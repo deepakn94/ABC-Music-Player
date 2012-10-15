fc036e759e17a5e42d47bd10bfed13773fb762fd
Deepak <Deepak@18.189.72.107>
2012-10-15 12:14:40 -0400

    Added enums HeaderTokenTypes and BodyTokenTypes to Token.java

--- abcPlayer/src/sound/Token.java
+++ abcPlayer/src/sound/Token.java
@@ -1,10 +1,50 @@
 package sound;
 
 /*
- * Tokens used -->
+ * Tokens used
  * 
+ * HeaderTokens -->
+ * 
+ * Title
+ * Composer's name
+ * Meter
+ * Tempo 
+ * Voice 
+ * Key
+ * Index Number
+ * 
+ * 
+ * BodyTokens -->
+ * 
+ * Note
+ * Rest
+ * Chords
+ * Tuplets
+ * Repeats
+ * Barlines
+ * Change of voice token
+ * End of line
  * 
  */
 public class Token {
+	public static enum HeaderTokenTypes {
+		TITLE,
+		COMPOSER_NAME,
+		METER,
+		TEMPO,
+		VOICE,
+		KEY,
+		INDEX_NUMBER
+	}
 	
+	public static enum BodyTokenTypes {
+		NOTE,
+		REST,
+		CHORD,
+		TUPLET,
+		REPEAT,
+		BARLINE,
+		VOICE_CHANGE,
+		END_OF_LINE
+	}
 }
