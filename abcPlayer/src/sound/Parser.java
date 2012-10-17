60774eb4a4f1cc25b69616ad4c7dea5d6d8e3d8b
Deepak <Deepak@18.189.72.107>
2012-10-16 23:40:38 -0400

    Made formatting changes to the Parser.

--- abcPlayer/src/sound/Parser.java
+++ abcPlayer/src/sound/Parser.java
@@ -1,7 +1,8 @@
 package sound;
 
 public class Parser {
-
+    Lexer lex;
+    
     /**
      * Creates a new parser over the lexer.  This parser will use the passed
      * lexer to get tokens--which it will then parse.
@@ -9,8 +10,6 @@ public class Parser {
      * @param lexer The lexer.
      * @return none
      */
-    
-    Lexer lex;
     public Parser(Lexer lexer) {
         this.lex = lexer;     
     }
