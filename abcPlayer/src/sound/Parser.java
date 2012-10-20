c45d5dc54cc9d7ca5be65b14d1074006187a4a9c
Deepak <Deepak@18.189.99.74>
2012-10-20 17:34:16 -0400

    Added helper methods to Parser.

--- abcPlayer/src/sound/Parser.java
+++ abcPlayer/src/sound/Parser.java
@@ -1,5 +1,8 @@
 package sound;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Parser {
     Lexer lex;
     
@@ -18,6 +21,76 @@ public class Parser {
     {
         
         
+    }
+    
+    private Accidental getAccidental(String noteToken) {
+    	String ACCIDENTAL_REGEX = "\\^\\^?|__?|=";
+    	Pattern accidentalPattern = Pattern.compile(ACCIDENTAL_REGEX);
+    	Matcher accidentalMatcher = accidentalPattern.matcher(noteToken);
+    	String accidental = accidentalMatcher.find() ? accidentalMatcher.group(0) : "";
+    	switch (accidental) {
+    		case "_": return Accidental.FLAT; 
+    		case "__": return Accidental.DOUBLEFLAT;
+    		case "^": return Accidental.SHARP;
+    		case "^^": return Accidental.DOUBLESHARP;
+    		case "=": return Accidental.NATURAL;
+    		default: return Accidental.NATURAL;
+    	}
+    }
+    
+    private NoteType getNote(String noteToken) {
+    	String NOTE_REGEX = "[A-Ga-g]";
+    	Pattern notePattern = Pattern.compile(NOTE_REGEX);
+    	Matcher noteMatcher = notePattern.matcher(noteToken);
+    	String note = noteMatcher.find() ? noteMatcher.group(0) : "";
+    	switch (note) {
+    		case "A": 
+    		case "a":
+    			return NoteType.A; 
+    		case "B": 
+    		case "b":
+    			return NoteType.B;
+    		case "C":
+    		case "c":
+    			return NoteType.C;
+    		case "D": 
+    		case "d":
+    			return NoteType.D;
+    		case "E":
+    		case "e":
+    			return NoteType.E;
+    		case "F":
+    		case "f":
+    			return NoteType.F;
+    		case "G":
+    		case "g":
+    			return NoteType.G;
+    		default:
+    			throw new IllegalArgumentException("Illegal Note");
+    	}
+    }
+    
+    private int getOctave(String noteToken) {
+    	String HIGHER_OCTAVE_REGEX = "[a-g]";
+    	String OCTAVE_REGEX = "('+)|(,+)";
+    	Pattern higherOctave = Pattern.compile(HIGHER_OCTAVE_REGEX);
+    	Pattern octavePattern = Pattern.compile(OCTAVE_REGEX);
+    	
+    	Matcher higherOctaveMatcher = higherOctave.matcher(noteToken);
+    	Matcher octaveMatcher = octavePattern.matcher(noteToken);
+    	int octave = higherOctaveMatcher.find() ? 1 : 0;
+    	String octaves = octaveMatcher.find() ? octaveMatcher.group(0) : "";
+    	
+    	if (octaveMatcher.group(1) != null) octave += octaves.length();
+    	if (octaveMatcher.group(2) != null) octave -= octaves.length();
+    	
+    	return octave;
+    }
+    
+    public void ParseNote(String noteToken) {
+    	Accidental noteAccidental = getAccidental(noteToken);
+    	NoteType noteName = getNote(noteToken);
+    	int octave = getOctave(noteToken);
     }
     
 }
